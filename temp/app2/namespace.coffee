656ca31c581d0d49f22bfc305559dc2a43f0cdc2
Joe Prempeh <joe@staance.com>
2015-10-21 12:45:36 -0700

    chpt 2 creating namespaces done

--- temp/app2/namespace.coffee
+++ temp/app2/namespace.coffee
@@ -2,6 +2,18 @@ io = require 'socket.io'
   .listen 4000
 
 # Listeners
+# python -m SimpleHTTPServer
+
+io.sockets.on 'connection', (socket) ->
+  socket.on 'join', (data) ->
+    socket.username = data.username
+    socket.broadcast.emit 'join', username: data.username, socket: socket.id
+
+  socket.on 'ping', () ->
+    socket.broadcast.emit 'ping', username: socket.username
+
+  socket.on 'privatePing', (data) ->
+    io.sockets.connected[data.socket].emit 'ping', username: socket.username, priv: true
 
 io.of('/vip').on 'connection', (socket) ->
   socket.on 'join', (data) ->
--- temp/app2/namespace.js
+++ temp/app2/namespace.js
@@ -4,6 +4,27 @@
 
   io = require('socket.io').listen(4000);
 
+  io.sockets.on('connection', function(socket) {
+    socket.on('join', function(data) {
+      socket.username = data.username;
+      return socket.broadcast.emit('join', {
+        username: data.username,
+        socket: socket.id
+      });
+    });
+    socket.on('ping', function() {
+      return socket.broadcast.emit('ping', {
+        username: socket.username
+      });
+    });
+    return socket.on('privatePing', function(data) {
+      return io.sockets.connected[data.socket].emit('ping', {
+        username: socket.username,
+        priv: true
+      });
+    });
+  });
+
   io.of('/vip').on('connection', function(socket) {
     socket.on('join', function(data) {
       socket.username = data.username;
